d0525e3cdb7a8498e59e2cc68aba9389390d0e93
NewBediver <serikov_v1999@mail.ru>
2020-07-17 19:11:31 +1000

    Use of textures.

---
+++ OpenGLTraining/Shaders/5.1.Transformation.vs
@@ -0,0 +1,17 @@
+#version 330 core
+
+layout (location = 0) in vec3 a_Pos;
+layout (location = 1) in vec3 a_Color;
+layout (location = 2) in vec2 a_TextureCoord;
+
+uniform mat4 transform;
+
+out vec3 ourColor;
+out vec2 TextureCoord;
+
+void main()
+{
+	gl_Position = transform * vec4(a_Pos, 1.0);
+	ourColor = a_Color;
+	TextureCoord = a_TextureCoord;
+}
---
+++ OpenGLTraining/src/Transformations.cpp
@@ -0,0 +1,263 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "Texture/StbBuild.h"
+#include "Shader/Shader.h"
+
+#include <iostream>
+
+float visibility;
+
+// Callback for resizing
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+// Input processing
+void processInput(GLFWwindow* window);
+// Update the third parameter of mix function
+void updateVisibility(float updateValue);
+
+int main()
+{
+	// Initialize OpenGL and configure it
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create GLFWwindow and configure context
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create OpenGL window!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD!" << std::endl;
+		return -1;
+	}
+
+	// Get the maximum number of 4 component vertex attributes
+	int nrAttributes;
+	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+	std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+
+	// Configure viewport
+	glViewport(0, 0, 800, 600);
+
+	// Setup resize callback for window
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	//======================================
+
+	float vertices[] = {
+		// positions            // colors              // texture coords
+		 0.5f,  0.5f, 0.0f,     1.0f, 0.0f, 0.0f,      1.0f, 1.0f, // top right
+		 0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,      1.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f,     0.0f, 0.0f, 1.0f,      0.0f, 0.0f, // bottom left
+		-0.5f,  0.5f, 0.0f,     1.0f, 1.0f, 0.0f,      0.0f, 1.0f  // top left
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3, // First triangle
+		1, 2, 3  // Second triangle
+	};
+
+	// Create shader
+	Shader ourShader("Shaders/5.1.Transformation.vs", "Shaders/5.1.Transformation.fs");
+
+	unsigned int VAO, VBO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Set positions
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(0));
+	glEnableVertexAttribArray(0);
+	// Set colors
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	// Set texture coords
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	// Generate texture object
+	unsigned int texture1;
+	glGenTextures(1, &texture1);
+
+	// Bind texture
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	// set the texture wrapping/filtering options (on currently bound texture)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// Say stb to flip texture vertically
+	stbi_set_flip_vertically_on_load(true);
+
+	// Get texture image data
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("Textures/container.jpg", &width, &height, &nrChannels, 0);
+
+	if (data)
+	{
+		// Generate texture and create it's bitmap
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture1" << std::endl;
+	}
+
+	// Generate texture object
+	unsigned int texture2;
+	glGenTextures(1, &texture2);
+
+	// Bind texture
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	// set the texture wrapping/filtering options (on currently bound texture)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// Get texture image data
+	data = stbi_load("Textures/awesomeface.png", &width, &height, &nrChannels, 0);
+
+	if (data)
+	{
+		// Generate texture and create it's bitmap
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture2" << std::endl;
+	}
+
+	// Free image data because we have already set it to our texture
+	stbi_image_free(data);
+
+	// Set texture units to its uniforms
+	ourShader.Use();
+	ourShader.SetInt("texture1", 0);
+	ourShader.SetInt("texture2", 1);
+
+	visibility = 0.2f;
+
+	//======================================
+
+	// Render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Check input
+		processInput(window);
+
+		// State-setting function setup color
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		// State-using function uses current state to retrieve the color
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//========================================
+
+		// Bind textures on corresponding texture units
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+
+		glUniform1f(glGetUniformLocation(ourShader.GetID(), "visibility"), visibility);
+
+		// Lower right container -------------------------
+
+		// Scale, rotate and then translate
+		glm::mat4 trans = glm::mat4(1.0f);
+		trans = glm::translate(trans, glm::vec3(0.65f, -0.65f, 0.0f));
+		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+		trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 1.0f));
+
+		glUniformMatrix4fv(glGetUniformLocation(ourShader.GetID(), "transform"), 1, GL_FALSE, glm::value_ptr(trans));
+
+		// render container
+		ourShader.Use();
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		// -------------------------------------------------
+
+		// Upper left container ----------------------------
+
+		// Scale then translate
+		trans = glm::mat4(1.0f);
+		trans = glm::translate(trans, glm::vec3(-0.5f, 0.5f, 0.0f));
+		trans = glm::scale(trans, glm::vec3(abs(sin(glfwGetTime())), abs(sin(glfwGetTime())), 1.0f));
+		
+		glUniformMatrix4fv(glGetUniformLocation(ourShader.GetID(), "transform"), 1, GL_FALSE, glm::value_ptr(trans));
+
+		// draw container
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		// ---------------------------------------------------
+
+		//=========================================
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		// Check and call events
+		glfwPollEvents();
+	}
+
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
+	// Properly cleaning / deleting all the GLFW resources
+	glfwTerminate();
+	return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+	else if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+	{
+		updateVisibility(0.001f);
+	}
+	else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+		updateVisibility(-0.001f);
+	}
+}
+
+void updateVisibility(float updateValue) {
+	if (visibility + updateValue > 1.0f || visibility + updateValue < 0.0f) return;
+	visibility += updateValue;
+}
